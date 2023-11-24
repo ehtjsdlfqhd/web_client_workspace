35e63831c9e495c95edab13cab2c7a02b411e3d3
ehtjsdlfqhd <ehtjsdlfqhd@naver.com>
2023-11-24 09:18:14 +0900

    2023-11-22실습문제2번 복기중

--- practice/practice_js/practice20231122_2.js
+++ practice/practice_js/practice20231122_2.js
@@ -13,3 +13,38 @@ const info = ([name, gender, ...score]) => {
     const avg = Math.round((sum / score.length) * 10) / 10;
     console.log(`${name}, ${gender} 의 평균점수는 ${avg}`);
 }
+
+const test2 = () => {
+    const people = [  
+        {
+              name : '홍길동',
+              family : {
+                  father : '홍석천',
+                  mother : '홍진경',
+                  sister : '홍수아',
+              }, 
+              age : 35
+        },
+        {
+              name : '신사임당',
+              family : {
+                  father : '신동엽',
+                  mother : '고두심',
+                  brother : '신하균',
+              }, 
+              age : 25
+        },
+          {
+              name : '이순신',
+              family : {
+                  father : '이경영',
+                  mother : '이영자',
+                  brother : '이승환',
+                  sister : '아이유',
+              }, 
+              age : 33
+        }
+      ]
+    const members = [];
+    members.push()
+}
