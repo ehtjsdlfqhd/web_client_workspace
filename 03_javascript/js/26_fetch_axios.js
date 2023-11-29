99c9f4fe8c5a7dcc67b311dd0dbeb5dfff3dff20
ehtjsdlfqhd <ehtjsdlfqhd@naver.com>
2023-11-29 15:30:39 +0900

    2023-11-29 수업

--- 03_javascript/js/26_fetch_axios.js
+++ 03_javascript/js/26_fetch_axios.js
@@ -6,6 +6,10 @@
  * - json 응답에 대한 처리
  *  - json이란? javascript object notation, 프로그램간의 데이터 교환언어(약속)
  *  - {"id":"shqkel","company":"KH정보교육원","classroom":"352","cnt":79}
+ *  - 속성명은 반드시 쌍따옴표로 감싼다. ''금지
+ *  - 문자열 역시 반드시 쌍따옴표로 감싼다.
+ *  - 숫자/boolean/null은 그대로 작성
+ *  - 객체/배열 사용가능
  */
 document.querySelector("#btn1").addEventListener('click', () => {
     const url = 'https://asia-northeast3-focal-elf-326215.cloudfunctions.net/user';
@@ -81,4 +85,38 @@ document.querySelector('#btn4').addEventListener('click', () => {
             img.height = 100;
             document.querySelector('.img-wrapper').append(img);
         });
+        
+});
+
+
+/**
+ * API KEY와 함께 전송하기
+ * - 보통 API는 전송량 제한을 위해 key값을 가지고 요청하게 된다.
+ * - http요청 헤더부분에 지정한 헤더명으로 등록해야 한다.
+ */
+document.querySelector('#btn5').addEventListener('click', () => {
+    const url = 'https://api.thecatapi.com/v1/images/search';
+
+    // axios(url, options)
+    axios(url, {
+        headers : {
+            'x-api-key' : 'live_ueMgvhuuhcBv4EmErlQjBT8XRO0dcy4z2SavjPtzvEFPilsfihkBiZTShB3MTerI'
+        },
+        params : {
+            limit : 1, // 기본값
+        }
+    })
+    .then((response) => {
+        console.log(response);
+        // response > data > [{url}]
+        // .cat-wrapper > img
+        const {data:[{url}]} = response;
+        console.log(url);
+        const img = document.createElement('img');
+        img.src = url;
+        img.style = 'width:200px;';
+        const wrapper = document.querySelector(".cat-wrapper");
+        wrapper.innerHTML = '';
+        wrapper.append(img);
+    })
 });
