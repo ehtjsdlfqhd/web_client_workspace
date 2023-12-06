16617258caac74681e92526a0ae9b59aae080a8e
ehtjsdlfqhd <ehtjsdlfqhd@naver.com>
2023-12-06 09:49:42 +0900

    tailwindcss 수업

--- 06_tailwindcss/js/tailwind.config.js
+++ 06_tailwindcss/js/tailwind.config.js
@@ -3,6 +3,9 @@ tailwind.config = {
       extend: {
         colors: {
           clifford: '#da373d',
+        },
+        fontFamily : {
+          'sans' : ['궁서', 'sans-serif'] //기본서체 설정
         }
       }
     }
